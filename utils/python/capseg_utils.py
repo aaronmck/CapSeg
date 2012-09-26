e6d766e33896411c1acacdaba0431b04f9dc3285
Aaron McKenna <aaronmck@wu.edu>
2012-09-26 16:39:30 -0700

    adding some debug info

--- utils/python/capseg_utils.py
+++ utils/python/capseg_utils.py
@@ -37,10 +37,9 @@ def process_bait_factors(targets,coverage_managers,stats_filename,target_pos):
         for sample,cov_manager in coverage_managers.iteritems():
             if cov_manager.get_current_tag() == target:
                 coverage.extend(cov_manager.get_coverage())
-                cov_manager.next()
             else:
                 coverage.extend([0]*cov_manager.good_lane_count())
-
+            cov_manager.next()
         # calculate a couple of statistics on each bait
         bf = numpy.median(coverage)
         entropy = stats.distributions.entropy(coverage)
